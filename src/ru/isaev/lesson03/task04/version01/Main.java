8a5f3dbbac68d39ed7840f8cdfeea1010c0f4c66
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-03-24 14:03:04 +1100

    Выполнение домашнего задания по 3 лекции.
    Добавление еще одной версии исполнения ДЗ. Распределение по папкам.
    Также устранение выявленных ошибок в логике.

--- src/ru/isaev/lesson03/task04/version01/Main.java
+++ src/ru/isaev/lesson03/task04/version01/Main.java
@@ -1,4 +1,4 @@
-package ru.isaev.lesson03.task04;
+package ru.isaev.lesson03.task04.version01;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -8,8 +8,10 @@ public class Main
     public static void main(String[] args)
     {
         //region Инициализация данных
+        final byte maxValue = 100;                  // Максимально загаданное число
         final byte randomNumber;                    // Загаданное программой число, которое необходимо отгадать
         boolean gameOver = false;                   // Переменная контроля окончания игры
+        boolean inGame = false;                     // Контроль что игрок вошел в игру
         byte memory = 0;                            // Последнее введеное число игроком
         Scanner input = new Scanner(System.in);     // Инициализация средства приема данных с консоли
         String inputLine;                           // Инициализация переменной входных значений с консоли
@@ -18,12 +20,12 @@ public class Main
 
         {   // Генерация случайного значения от 0 до 100 в randomNumber
             Random rand = new Random();
-            randomNumber = (byte) rand.nextInt(101);
+            randomNumber = (byte) rand.nextInt(maxValue + 1);
         }   // Освобождение памяти от ненужного в дальнейшем объекта Random
 
         //region Приветствие игрока и правила игры
         System.out.println("Добро пожаловать в игру \"горячо\", \"холодно\".");
-        System.out.println("Приложение загадало число. Отгадайте какое число было загадано.");
+        System.out.println("Приложение загадало число от 0 до "+ maxValue +". Отгадайте какое число было загадано.");
         System.out.print("Чтобы выйти из игры введите \"exit\". Введите ваш ответ:");
         //endregion
 
@@ -54,7 +56,12 @@ public class Main
 
             if (!gameOver)
             {
-                if (number >= memory & number < randomNumber)
+                if (memory == 0 & !inGame)
+                {
+                    memory = (byte) number;
+                    inGame = true;
+                }
+                if (number > memory & number < randomNumber)
                 {
                     System.out.println("Горячо");
                     gameOver = true;
@@ -69,7 +76,7 @@ public class Main
                     System.out.println("Холодно");
                     memory = (byte) number;
                 }
-                else if (number <= memory & number > randomNumber)
+                else if (number < memory & number > randomNumber)
                 {
                     System.out.println("Горячо");
                     gameOver = true;
