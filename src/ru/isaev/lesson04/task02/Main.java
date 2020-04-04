bc147b73fad05e6801d261e95f1a56508462de51
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-04-04 20:59:27 +1100

    Reformat code

--- src/ru/isaev/lesson04/task02/Main.java
+++ src/ru/isaev/lesson04/task02/Main.java
@@ -2,12 +2,10 @@ package ru.isaev.lesson04.task02;
 
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     static Scanner input = new Scanner(System.in);  // Инициализация средства приема данных с консоли
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //region Инициализация данных
         int number;                                 // Число, которое необходимо определить
         //endregion
@@ -21,49 +19,39 @@ public class Main
         WhatIsIt(number);
         //endregion
     }
+
     // Прием чисел с консоли
-    private static int InputFromConsole()
-    {
+    private static int InputFromConsole() {
         //region Данные метода
         String inputLine;                           // Данные введенные с консоли
         //endregion
 
-        while (true)
-        {
+        while (true) {
             inputLine = input.next();
-            try
-            {
+            try {
                 return Integer.parseInt(inputLine);
-            }
-            catch (NumberFormatException e)
-            {
+            } catch (NumberFormatException e) {
                 System.out.println("Пожалуйста введите число.");
             }
         }
     }
 
     // Определение введенного числа
-    private static void WhatIsIt (int number)
-    {
-        if (number == 0)
-        {
+    private static void WhatIsIt(int number) {
+        if (number == 0) {
             System.out.println("Введенное число равно нулю");
-        }
-        else if (number > 0)
-        {
+        } else if (number > 0) {
             System.out.println("Введенное число положительное");
-        }
-        else
-        {
+        } else {
             System.out.println("Введенное число отрицательное");
         }
 
-        if (number != 0)
-        {
-            if (number % 2 == 0)
-        { System.out.println("Оно чётное"); }
-            else
-        { System.out.println("Оно нечётное"); }
+        if (number != 0) {
+            if (number % 2 == 0) {
+                System.out.println("Оно чётное");
+            } else {
+                System.out.println("Оно нечётное");
+            }
         }
     }
 }
