1c1928e3877f6a91634e46a45217a24c2f9bf4b5
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-04-04 20:59:58 +1100

    Reformat code

--- src/ru/isaev/lesson04/task03/Main.java
+++ src/ru/isaev/lesson04/task03/Main.java
@@ -1,9 +1,7 @@
 package ru.isaev.lesson04.task03;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         //region Инициализация данных
         final byte minNum = 1;                                      // Начальное число
         final byte maxNum = 10;                                     // Конечное число
@@ -17,23 +15,18 @@ public class Main
     }
 
     // Заполнение массива
-    private static void CreateNum(byte minNum, byte maxNum, byte[] numbers)
-    {
+    private static void CreateNum(byte minNum, byte maxNum, byte[] numbers) {
         byte n = 0;
-        for (byte i = minNum; i <= maxNum; i++)
-        {
+        for (byte i = minNum; i <= maxNum; i++) {
             numbers[n] = i;
             n++;
         }
     }
 
     // Вывод таблицы умножения на экран
-    private static void MultiTable(byte[] numbers)
-    {
-        for (byte i: numbers)
-        {
-            for (byte j: numbers)
-            {
+    private static void MultiTable(byte[] numbers) {
+        for (byte i : numbers) {
+            for (byte j : numbers) {
                 byte result = (byte) (i * j);
                 System.out.println(i + " * " + j + " = " + result);
             }
