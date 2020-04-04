b7e2e8ec7f85237030ccb6a5a9dd35228fd0c806
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-04-04 20:59:14 +1100

    Reformat code

--- src/ru/isaev/lesson04/task01/Main.java
+++ src/ru/isaev/lesson04/task01/Main.java
@@ -2,23 +2,20 @@ package ru.isaev.lesson04.task01;
 
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     static Scanner input = new Scanner(System.in);  // Инициализация средства приема данных с консоли
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //region Инициализация данных метода
         final byte numbers = 2;                     // Сколько всего будет на прием чисел
         int[] arrNumbers = new int[numbers];        // Инициализация переменной входных значений с консоли
         //endregion
 
         //Приветствие
-        System.out.println("Добро пожаловать в программу нахождения минимального из "+ numbers +" чисел");
+        System.out.println("Добро пожаловать в программу нахождения минимального из " + numbers + " чисел");
 
         //region Основной алгоритм
-        for (int i = 0; i < numbers; i++)
-        {
+        for (int i = 0; i < numbers; i++) {
             int num = i + 1;
             System.out.println("Введите " + num + " число: ");
             InputFromConsole(arrNumbers, i);
@@ -28,34 +25,27 @@ public class Main
     }
 
     // Прием чисел с консоли
-    private static void InputFromConsole(int[] arrNumbers, int i)
-    {
+    private static void InputFromConsole(int[] arrNumbers, int i) {
         //region Данные метода
         String inputLine;                           // Данные введенные с консоли
         boolean allDone = false;                    // Контроль окончания ввода чисел
         //endregion
 
-        while (!allDone)
-        {
+        while (!allDone) {
             inputLine = input.next();
-            try
-            {
+            try {
                 arrNumbers[i] = Integer.parseInt(inputLine);
                 allDone = true;
-            }
-            catch (NumberFormatException e)
-            {
+            } catch (NumberFormatException e) {
                 System.out.println("Пожалуйста введите число.");
             }
         }
     }
 
     // Алгоритм нахождения минимального числа
-    private static int MinimalNumber(int[] numbers)
-    {
+    private static int MinimalNumber(int[] numbers) {
         int num = numbers[0];
-        for (int i: numbers)
-        {
+        for (int i : numbers) {
             num = Math.min(i, num);
         }
         return num;
