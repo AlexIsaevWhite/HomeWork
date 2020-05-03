1696a3eeed0a7f47f33fad9dcdeda304aaa2ab29
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-05-03 16:39:55 +1100

    Устранение выявленных замечаний: Замена File dir на Path dir, Переименован recursion на printContent, Замена String depth на int depth, Мелкие исправления.

--- src/main/java/ru/isaev/lesson15/FileSystem.java
+++ src/main/java/ru/isaev/lesson15/FileSystem.java
@@ -3,31 +3,33 @@ package ru.isaev.lesson15;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileSystem {
-    final private static String SEPARATOR = File.separator + File.separator;
+    final private static String SEPARATOR = File.separator;
 
     public static void main(String[] args) {
         new FileSystem().startWork();
     }
 
     void startWork() {
-        File dir = new File("target" + SEPARATOR + "classes" + SEPARATOR + "ru" + SEPARATOR + "isaev"
+        Path dir = Paths.get("target" + SEPARATOR + "classes" + SEPARATOR + "ru" + SEPARATOR + "isaev"
                 + SEPARATOR + "lesson15" + SEPARATOR + "test");
-        File dir2 = new File(dir, "new");
-        File newFile1 = new File(dir, "MyObject.obj");
-        File newFile2 = new File(dir2, "MyObject2.obj");
-        File renamedFile = new File(dir, "RenamedMyObject.obj");
-        File duplicateFile = new File(dir, "DuplicateMyObject.obj");
+        Path dir2 = Paths.get(dir + SEPARATOR + "new");
+        File newFile1 = new File(dir.toString(), "MyObject.obj");
+        File newFile2 = new File(dir2.toString(), "MyObject2.obj");
+        File renamedFile = new File(dir.toString(), "RenamedMyObject.obj");
+        File duplicateFile = new File(dir.toString(), "DuplicateMyObject.obj");
 
-        if (createDir(dir2) && createFile(newFile1)) {
+        if (createDir(dir2.toFile()) && createFile(newFile1)) {
             copy(newFile1, duplicateFile);
             rename(duplicateFile, renamedFile);
             copy(newFile1, duplicateFile);
             delete(newFile1);
             createFile(newFile2);
         }
-        recursion(dir, "");
+        printContent(dir.toFile());
     }
 
     private boolean createFile(File newFile) {
@@ -35,20 +37,19 @@ public class FileSystem {
             if (newFile.createNewFile()) {
                 System.out.println("Файл " + newFile.getName() + " создан.");
                 return true;
-            } else
-                return false;
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
         }
+        return false;
     }
 
     private boolean createDir(File dir) {
         if (dir.mkdirs()) {
             System.out.println("Директория " + dir.getAbsolutePath() + " создана");
             return true;
-        } else
-            return false;
+        }
+        return false;
     }
 
     private void copy(File file, File duplicate) {
@@ -61,8 +62,9 @@ public class FileSystem {
     }
 
     private void rename(File file, File renamedFile) {
-        if (file.renameTo(renamedFile))
+        if (file.renameTo(renamedFile)) {
             System.out.println("Файл " + file.getName() + " переименован в " + renamedFile.getName());
+        }
     }
 
     private void delete(File file) {
@@ -74,20 +76,46 @@ public class FileSystem {
         }
     }
 
-    private void recursion(File dir, String depth) {
+    private void printContent(File dir) {
         if (dir.exists()) {
             File[] files = dir.listFiles();
-            String path = depth + File.separator + dir.getName() + File.separator;
-            depth = new String(new char[path.length()]);
+            String path = SEPARATOR + dir.getName() + SEPARATOR;
             if (files != null) {
                 for (File file : files) {
                     if (file.isFile()) {
                         System.out.println(path);
-                        System.out.println(depth + file.getName());
-                    } else
-                        recursion(file, depth);
+                        showDepth(path.length());
+                        System.out.println(file.getName());
+                    } else {
+                        printContent(file, path.length());
+                    }
                 }
             }
+        }
+    }
+
+    private void printContent(File dir, int depth) {
+        if (dir.exists()) {
+            File[] files = dir.listFiles();
+            String path = SEPARATOR + dir.getName() + SEPARATOR;
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        showDepth(depth);
+                        System.out.println(path);
+                        showDepth(depth + path.length());
+                        System.out.println(file.getName());
+                    } else {
+                        printContent(file, depth + path.length());
+                    }
+                }
+            }
+        }
+    }
+
+    private void showDepth(int depth) {
+        for (int i = 0; i < depth; i++) {
+            System.out.print(' ');
         }
     }
 }
