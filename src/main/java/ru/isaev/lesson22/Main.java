c7a312ecad5ce96a7c4d50b4974787f5caf81ce8
Alexander Isaev <A_Isaev@mail.ru>
2020-05-18 10:25:43 +1100

    Оптимизация кода.

--- src/main/java/ru/isaev/lesson22/Main.java
+++ src/main/java/ru/isaev/lesson22/Main.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> personList = new ArrayList<>();
-        personList.add(new Person(25, "Михаил"));
-        personList.add(new Person(18, "Анастасия"));
-        personList.add(new Person(19, "Шахерезада"));
-        personList.add(new Person(20, "Евгений"));
-        personList.add(new Person(20, "Михаил"));
-        personList.add(new Person(18, "Авраам"));
-        personList.sort(new PersonSuperComparator());
+        List<Person> personList = new ArrayList<Person>() {
+            {
+                add(new Person(25, "Михаил"));
+                add(new Person(18, "Анастасия"));
+                add(new Person(19, "Шахерезада"));
+                add(new Person(20, "Евгений"));
+                add(new Person(20, "Михаил"));
+                add(new Person(18, "Авраам"));
+                sort(new PersonSuperComparator());
+            }
+        };
         for (Person p : personList) {
             System.out.println(p.toString());
         }
