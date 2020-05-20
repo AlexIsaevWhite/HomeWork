0f0a415170bf2210d80c0ee7cb6d1c329103aece
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-05-20 21:35:22 +1100

    Инициализация домашней работы.

---
+++ src/main/java/ru/isaev/lesson25/task2/InternetBasket.java
@@ -0,0 +1,25 @@
+package ru.isaev.lesson25.task2;
+
+public class InternetBasket {
+    /*
+    25_ДЗ_2_Корзина с использованием мапы
+Реализовать класс корзины интернет магазина по следующему интерфейсу, используя
+реализацию мапы:
+
+interface Basket {
+
+    void addProduct(String product, int quantity);
+
+    void removeProduct(String product);
+
+    void updateProductQuantity(String product, int quantity);
+
+    void clear();
+
+    List<String> getProducts();
+
+    int getProductQuantity(String product);
+
+}
+     */
+}
