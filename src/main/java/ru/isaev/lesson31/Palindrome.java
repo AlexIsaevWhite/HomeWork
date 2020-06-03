d34905f15c8b05a53c97506e85fd20f861d24e36
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-06-03 19:27:14 +1100

    Грамматические исправления.

--- src/main/java/ru/isaev/lesson31/Palindrome.java
+++ src/main/java/ru/isaev/lesson31/Palindrome.java
@@ -3,31 +3,31 @@ package ru.isaev.lesson31;
 public class Palindrome {
 
     public static void main(String[] args) {
-        String[] string = {"12121", "abba", "addidas"};
+        String[] string = {"12121", "abba", "adidas", "madam"};
         for (String s : string) {
             System.out.println(s);
-            System.out.println("isPolidrome: " + isPolidrome(s));
-            System.out.println("isPolidromeStandartFunction: " + isPolidromeStandartFunction(s));
+            System.out.println("isPalindrome: " + isPalindrome(s));
+            System.out.println("isPalindromeStandardFunction: " + isPalindromeStandardFunction(s));
             System.out.println();
         }
     }
 
-    static boolean isPolidrome(String input) {
-        boolean isPolidrome;
+    static boolean isPalindrome(String input) {
+        boolean isPalindrome;
         if (input != null) {
-            isPolidrome = true;
+            isPalindrome = true;
             char[] chars = input.toCharArray();
             for (int begin = 0, end = chars.length - 1; begin < end; begin++, end--) {
                 if (chars[begin] != chars[end]) {
-                    isPolidrome = false;
+                    isPalindrome = false;
                     break;
                 }
             }
-        } else isPolidrome = false;
-        return isPolidrome;
+        } else isPalindrome = false;
+        return isPalindrome;
     }
 
-    static boolean isPolidromeStandartFunction(String input) {
+    static boolean isPalindromeStandardFunction(String input) {
         StringBuilder buffer = new StringBuilder(input);
         buffer.reverse();
         String data = buffer.toString();
