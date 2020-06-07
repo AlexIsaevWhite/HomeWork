68bf3d2858e1706c1facb15b1cf7a0af28d57f25
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-06-07 16:53:42 +1100

    Инициализация 33 ДЗ.

---
+++ src/main/java/ru/isaev/lesson33/MaxCharNum.java
@@ -0,0 +1,11 @@
+package ru.isaev.lesson33;
+
+public class MaxCharNum {
+    /*
+    ДЗ 33. Максимально встречающийся символ в строке.
+Вывести максимально встречающийся символ в строке.
+Пример:
+This is test message
+Character: s has occurred maximum times in String: 5
+     */
+}
