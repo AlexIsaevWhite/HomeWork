5dbf8d2ef732e9179a3976875b98e8b66c167103
Alexander Isaev <A_Isaev@mail.ru>
2020-06-10 15:53:05 +1100

    Выполнение домашнего задания по 33 лекции.

--- src/main/java/ru/isaev/lesson33/MaxCharNum.java
+++ src/main/java/ru/isaev/lesson33/MaxCharNum.java
@@ -1,11 +1,37 @@
 package ru.isaev.lesson33;
 
+import java.util.*;
+
 public class MaxCharNum {
-    /*
-    ДЗ 33. Максимально встречающийся символ в строке.
-Вывести максимально встречающийся символ в строке.
-Пример:
-This is test message
-Character: s has occurred maximum times in String: 5
-     */
+
+    public static void main(String[] args) {
+        String s = "This is test message";
+        findMaxNumOfChar(s);
+    }
+
+    static void findMaxNumOfChar(String inputString) {
+        if (inputString != null) {
+            List<Character> arr = new ArrayList<>();
+            Set<Character> hs = new HashSet<>();
+            char maxChar = ' ';
+            int numMaxChar = 0;
+
+            for (char c : inputString.toCharArray()) {
+                arr.add(c);
+                hs.add(c);
+            }
+            for (char c : hs) {
+                int temp = Collections.frequency(arr, c);
+                if (temp > numMaxChar) {
+                    numMaxChar = temp;
+                    maxChar = c;
+                }
+            }
+            print(maxChar, numMaxChar);
+        }
+    }
+
+    static private void print(char c, int i) {
+        System.out.println("Character: " + c + " has occurred maximum times in String: " + i);
+    }
 }
