17bedee5555f1b6645bca59ba9917a9f34125e9b
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-06-15 21:55:42 +1100

    Рефакторинг кода

--- src/main/java/ru/isaev/lesson31/Palindrome.java
+++ src/main/java/ru/isaev/lesson31/Palindrome.java
@@ -1,6 +1,9 @@
 package ru.isaev.lesson31;
 
+import org.apache.log4j.Logger;
+
 public class Palindrome {
+    private final static Logger LOGGER = Logger.getLogger(Palindrome.class);
 
     public static void main(String[] args) {
         String[] string = {"12121", "abba", "adidas", "madam"};
@@ -12,16 +15,22 @@ public class Palindrome {
         }
     }
 
-    static boolean isPalindrome(String input) throws NullPointerException {
-        if (input == null) throw new NullPointerException();
+    static boolean isPalindrome(String input) {
+        if (input == null) {
+            LOGGER.warn("input is null");
+            return false;
+        }
         for (int begin = 0, end = input.length() - 1; begin < end; begin++, end--) {
             if (input.charAt(begin) != input.charAt(end)) return false;
         }
         return true;
     }
 
-    static boolean isPalindromeStandardFunction(String input) throws NullPointerException {
-        if (input == null) throw new NullPointerException();
+    static boolean isPalindromeStandardFunction(String input) {
+        if (input == null) {
+            LOGGER.warn("input is null");
+            return false;
+        }
         StringBuilder sb = new StringBuilder(input);
         String data = sb.reverse().toString();
         return input.equals(data);
