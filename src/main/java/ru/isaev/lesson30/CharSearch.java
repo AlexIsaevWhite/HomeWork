deaf7e89af42e9653cca26d13afb1a2a9fde7b1c
AlexIsaevWhite <a.isaev.stc@innopolis.university>
2020-06-15 21:58:28 +1100

    Рефакторинг кода

--- src/main/java/ru/isaev/lesson30/CharSearch.java
+++ src/main/java/ru/isaev/lesson30/CharSearch.java
@@ -1,10 +1,13 @@
 package ru.isaev.lesson30;
 
+import org.apache.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class CharSearch {
+    private final static Logger LOGGER = Logger.getLogger(CharSearch.class);
 
     public static void main(String[] args) {
         String[] str = {"total", "teter", "abracadabra"};
@@ -14,15 +17,16 @@ public class CharSearch {
         }
     }
 
-    static char searchOriginality(String inputString) throws NullPointerException {
-        if (inputString == null) throw new NullPointerException();
-        List<Character> charArr = new ArrayList<>();
-        for (char c : inputString.toCharArray()) {
-            charArr.add(c);
-        }
-        for (char c : charArr) {
-            if (Collections.frequency(charArr, c) == 1) return c;
-        }
+    static char searchOriginality(String inputString) {
+        if (inputString != null) {
+            List<Character> charArr = new ArrayList<>();
+            for (char c : inputString.toCharArray()) {
+                charArr.add(c);
+            }
+            for (char c : charArr) {
+                if (Collections.frequency(charArr, c) == 1) return c;
+            }
+        } else LOGGER.warn("inputString is null");
         return ' ';
     }
 }
